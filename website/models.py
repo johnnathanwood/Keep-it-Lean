7851e638ac436ecb1f9e72d8d1a6d71c8ba27f1a
John Wood <johnnathanwood@gmail.com>
2019-03-05 18:25:28 -0600

    created models

--- website/models.py
+++ website/models.py
@@ -1,13 +1,51 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# Business Model
+class Business(models.Model):
+    '''
+    This Model defines all of the relationships and attributes of the Business table.
+
+        Author: Group
+
+        Methods:
+            __str__ -- user
+    '''
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length= 50)
+
+    def __str__(self):
+        ''' returns a string representation of the model '''
+        return self.name
+
+# Product Model
 class Product(models.Model):
-    seller = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    title = models.CharField(max_length=255)
+    '''
+    This Model defines all the relationships and attributes of the Products table in the
+        database.
+
+        Author: Group
+
+        Methods:
+        __str__ -- title
+    '''
+    business = models.ForeignKey(Business, on_delete= models.PROTECT)
+    status = models.ForeignKey(Status, on_delete= models.PROTECT)
+    UPC = models.CharField(max_length=12)
     description = models.TextField(blank=True, null=True)
-    price = models.IntegerField()
-    quantity = models.IntegerField()
+
+    def __str__(self):
+        ''' returns a string representation of the model '''
+        return self.title
+
+
+class Status(models.Model):
+    '''
+    This Model defines the attributes of the Status table in the database.
+    '''
+    name = models.CharField(max_length= 50)
+
+    def __str__(self):
+        ''' returns a string representation of the model '''
+        return self.name
