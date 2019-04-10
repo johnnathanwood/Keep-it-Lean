cc9897f53fccbb3309c0d2303c469dbe984a35dc
John Wood <johnnathanwood@gmail.com>
2019-04-10 17:33:46 -0500

    joined staticfiles

--- bangazonweb/settings.py
+++ bangazonweb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 from django.conf.urls.static import static
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-django_heroku.settings(locals())
+
 
 
 
